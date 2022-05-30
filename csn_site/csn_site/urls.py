31ae155985dd1640b6cd223228ac95f520671386
Garyson <bbt1250912@gmail.com>
2022-05-30 17:19:42 +0900

    urls.py 수정

--- csn_site/csn_site/urls.py
+++ csn_site/csn_site/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('community/', include('community_page.urls')),
+    path('main/', include('main_page.urls')),
+    path('profile/', include('profile_page.urls')),
 ]
