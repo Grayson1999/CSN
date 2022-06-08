a3e7939f45e1379a94a1f3062894d9f97a7781a5
yubin <vundle34@naver.com>
2022-06-09 07:19:09 +0900

    게시글 추가, 수정 완성

--- csn_site/community_page/admin.py
+++ csn_site/community_page/admin.py
@@ -1,15 +1,17 @@
 from django.contrib import admin
-from .models import Post, Photo, Tag
+from .models import Post, Tag
+# from .models import Photo
 
 # Register your models here.
-class PhotoInline(admin.TabularInline):
-    model = Photo
+# class PhotoInline(admin.TabularInline):
+#     model = Photo
 
-class PostAdmin(admin.ModelAdmin):
-    inlines = [PhotoInline, ]
+# class PostAdmin(admin.ModelAdmin):
+#     inlines = [PhotoInline, ]
 
 class TagAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('name', )}
 
-admin.site.register(Post, PostAdmin)
+admin.site.register(Post)
+# admin.site.register(PostAdmin)
 admin.site.register(Tag, TagAdmin)
--- csn_site/community_page/urls.py
+++ csn_site/community_page/urls.py
@@ -5,5 +5,8 @@ from . import views
 urlpatterns = [
     path("",views.PostList.as_view(), name="community_page"),
     path("<int:pk>/", views.PostDetail.as_view()),
-    path('post_create/', views.post_create, name="post_create"),
+    # path('post_create/', views.post_create, name="post_create"),
+    path('post_create/', views.PostCreate.as_view(), name="post_create"),
+    # path('<int:pk>/post_update/', views.post_update, name="post_update"),
+    path('post_update/<int:pk>/', views.PostUpdate.as_view()),
 ]
