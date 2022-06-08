bbd8152751551455587094d22a6db664a89fa1a5
yubin <vundle34@naver.com>
2022-06-08 15:09:55 +0900

    게시글 작성 버튼 추가

---
+++ csn_site/community_page/forms.py
@@ -0,0 +1,18 @@
+#community_page/forms.py
+
+from django import forms
+from .models import Post
+
+class PostWriteForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(PostWriteForm, self).__init__(*args, **kwargs)
+        self.fields['title'].label = '제목'
+        self.fields['title'].widget.attrs.update({
+            'placeholder': '제목을 입력해주세요.',
+            'class': 'form-control',
+            'autofocus': True,
+        })
+
+    class Meta:
+        model = Post
+        fields = ['title', 'content']
