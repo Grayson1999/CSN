abba11f8c0d3256500d0fc85a89fc8e99df766f7
JuYeob Lee <lcg0116@gmail.com>
2022-06-08 21:37:18 +0900

    소셜 로그인 css 적용, 에러메세지 한글화

--- csn_site/common/forms.py
+++ csn_site/common/forms.py
@@ -5,6 +5,10 @@ from django.contrib.auth.models import User
 
 class UserForm(UserCreationForm):
     email = forms.EmailField(label="이메일")
+    password1 = forms.Field(label="비밀번호")
+    password2 = forms.Field(label="비밀번호 확인")
+    username = forms.Field(label="사용자 이름")
+
 
     class Meta:
         model = User
