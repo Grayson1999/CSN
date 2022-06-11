3252277577cf259193566d2ed36bbe31644f1c1f
yubin <vundle34@naver.com>
2022-06-11 18:50:38 +0900

    댓글 수정,삭제 기능 및 페이지네이션 기능 추가

--- csn_site/community_page/urls.py
+++ csn_site/community_page/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
+    path('comment_delete/<int:pk>/', views.CommentDelete),
     path("",views.PostList.as_view(), name="community_page"),
     path("<int:pk>/", views.PostDetail.as_view()),
     # path('post_create/', views.post_create, name="post_create"),
@@ -11,4 +12,5 @@ urlpatterns = [
     path('post_update/<int:pk>/', views.PostUpdate.as_view()),
     path('post_delete/<int:pk>/', views.PostDelete, name="post_delete"),
     path('<int:pk>/new_comment/', views.new_comment),
+    path('comment_update/<int:pk>/', views.CommentUpdate.as_view()),
 ]
--- csn_site/community_page/views.py
+++ csn_site/community_page/views.py
@@ -26,6 +26,18 @@ class PostList(ListView):
     model = Post
     ordering = '-pk'
     template_name = 'community_page/community.html'
+    paginate_by = 10
+    block_size = 5 # 하단의 페이지 목록 수
+
+    def get_context_data(self, **kwargs):
+        context = super(PostList, self).get_context_data(**kwargs)
+
+        start_index = int((context['page_obj'].number - 1) / self.block_size) * self.block_size
+        end_index = min(start_index + self.block_size, len(context['paginator'].page_range))
+
+        context['page_range'] = context['paginator'].page_range[start_index:end_index]
+
+        return context
 
 class PostDetail(DetailView):
     model = Post
@@ -75,7 +87,7 @@ class PostUpdate(LoginRequiredMixin, UpdateView):
 
     def dispatch(self, request, *args, **kwargs):
         if request.user.is_authenticated and request.user == self.get_object().author:
-            return super().dispatch(request, *args, **kwargs)
+            return super(PostUpdate, self).dispatch(request, *args, **kwargs)
         else:
             raise PermissionDenied
 
@@ -113,6 +125,17 @@ class PostUpdate(LoginRequiredMixin, UpdateView):
                     self.object.tags.add(tag)
         return response
 
+class CommentUpdate(LoginRequiredMixin, UpdateView):
+    model = Comment
+    form_class = CommentForm
+    template_name = "community_page/comment_update.html"
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated and request.user == self.get_object().author:
+            return super(CommentUpdate, self).dispatch(request, *args, **kwargs)
+        else:
+            raise PermissionDenied
+
 def PostDelete(request, pk):
     post = get_object_or_404(Post, pk=pk)
     author = post.author
@@ -122,6 +145,15 @@ def PostDelete(request, pk):
             return redirect('../../')
         else:
             return redirect(post.get_absolute_url())
+    else:
+        raise PermissionDenied
+
+def CommentDelete(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    post = comment.post
+    if request.user.is_authenticated and request.user == comment.author:
+        comment.delete()
+        return redirect(post.get_absolute_url())
     else:
         raise PermissionDenied
 
@@ -141,7 +173,6 @@ def new_comment(request, pk):
             return redirect(post.get_absolute_url())
     else:
         raise PermissionDenied
-
 
 # def create(request):
 #     if(request.method == 'POST'):
